5405384e76142bf055710916bab8dab67f50c843
Davi Fernandes <davifer.104@gmail.com>
2025-03-17 11:27:22 -0300

    arr review

---
+++ array/arrayReview.js
@@ -0,0 +1,24 @@
+//                         0       1       2         3
+const fruits = new Array("Maçã", "Uva", "Pêra", "Melancia");
+// delete fruits[3];
+const fruits2 = [...fruits];
+fruits2[2] = "Abacate";
+console.log(fruits);
+console.log(fruits2);
+
+const arraySlice = fruits.slice(0, -1);
+console.log("Array fatiado", arraySlice);
+
+console.log(fruits[fruits.length - 1]);
+
+const firstItem = fruits.shift();
+const lastItem = fruits.pop();
+
+console.log(firstItem, lastItem);
+console.log(fruits);
+
+const nameComplete = "Davi Fernandes de Lima";
+const arrName = nameComplete.split(" ");
+console.log(arrName);
+const stringName = arrName.join(" ");
+console.log(stringName);
